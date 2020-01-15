a2433de3c4ea3467858eb372d268bd86741b5240
Michael Palumbo <mlp95@psu.edu>
2020-01-15 14:30:08 -0500

    functions for planck

--- src/StellarAtmospheres.jl
+++ src/StellarAtmospheres.jl
@@ -1,5 +1,16 @@
+"""
+Author: Michael Palumbo
+Created: April 2019
+Contact: mlp95@psu.edu
+"""
+
 module StellarAtmospheres
 
-greet() = print("Hello World!")
+# bring in constants
+include("constants.jl")
+
+# define planck functions
+Bν(ν::t, T::t) where t<:Real = 2.0*h*ν^3/c^2 * 1.0/(exp(h*ν/(kB*T)) - 1.0)
+Bλ(λ::t, T::t) where t<:Real = 2.0*h*c^2/λ^5 * 1.0/(exp(h*c/(λ*kB*T)) - 1.0)
 
 end # module
---
+++ src/constants.jl
@@ -0,0 +1,4 @@
+# constants in cgs units
+c = 2.998e10
+h = 6.626e-27
+kB = 1.381e-16
