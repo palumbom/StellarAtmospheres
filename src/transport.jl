153ef32aa8f6542b31f6a1adf522eeb5076f4d5b
Michael Palumbo <mlp95@psu.edu>
2020-01-23 16:41:05 -0500

    docs

--- src/transport.jl
+++ src/transport.jl
@@ -1,3 +1,24 @@
-Sν_lin(τ::T, an::Vararg{T}) where T<:Real = an[1] + an[2] * τ
-Sν_quad(τ::T, an::Vararg{T}) where T<:Real = an[1] + an[2] * τ + an[3] * τ^2
-Cν(τ::T, Sν::Function, an::Vararg{T}) where T<:Real = Sν(τ, an...) * exp(-τ)
+"""
+    Sν_lin(τ, an...)
+
+Compute the linear source function at τ with coefficients a0 and a1.
+Coefficients should be passed as multiple arguments or a splatted array.
+"""
+Sν_lin(τ::T, an::T...) where T<:Real = an[1] + an[2] * τ
+
+"""
+    Sν_quad(τ, an...)
+
+Compute the quadratic source function at τ with coefficients a0, a1, a2.
+Coefficients should be passed as multiple arguments or a splatted array.
+"""
+Sν_quad(τ::T, an::T...) where T<:Real = an[1] + an[2] * τ + an[3] * τ^2
+
+"""
+    Cν(τ, an...)
+
+Compute the contribution function at τ with the specified source function and
+coefficients an.
+Coefficients should be passed as multiple arguments or a splatted array.
+"""
+Cν(τ::T, Sν::Function, an::T...) where T<:Real = Sν(τ, an...) * exp(-τ)
