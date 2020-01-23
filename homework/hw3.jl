3e22bf6e8e59c737c14d1180138c389c17bd5711
Michael Palumbo <mlp95@psu.edu>
2020-01-23 15:42:12 -0500

    contribution integrand

--- homework/hw3.jl
+++ homework/hw3.jl
@@ -10,13 +10,29 @@ outdir = "/Users/michael/Desktop/ASTRO530/figures/"
 mpl.style.use("atmospheres.mplstyle"); plt.ioff()
 
 # define some source functions
-Sν_lin(τ::T, an...) where T<:Real = an[1] .+ an[2] .* τ
-Sν_quad(τ::T, an...) where T<:Real = an[1] .+ an[2] .* τ .+ an[3] .* τ.^2
+Sν_lin(τ::T, an...) where T<:Real = an[1] + an[2] * τ
+Sν_quad(τ::T, an...) where T<:Real = an[1] + an[2] * τ + an[3] * τ^2
 
 # evaulate the source functions
-τ  = range(0.0, 100.0, length=1000)
+τ  = range(0.0, 10.0, length=1000)
 an = [1.0, 1.0, 1.0]
 S1 = Sν_lin.(τ, an...)
 S2 = Sν_quad.(τ, an...)
 
 # plot the source functions
+fig = plt.figure("Source Functions")
+ax1 = fig.add_subplot(111)
+ax1.plot(τ, S1, label="Linear")
+ax1.plot(τ, S2, label="Quadratic")
+ax1.set_xlabel(L"\tau_\nu")
+ax1.set_ylabel(L"S_\nu(\tau_\nu)")
+ax1.legend()
+fig.savefig(outdir*"hw3_source.pdf")
+plt.clf(); plt.close()
+
+# now define the contribution function integrand
+Cν(τ::T, Sν::Function, an...) where T<:Real = Sν(τ, an...) * exp(τ)
+
+# evaluate the contribution functions
+C1 = Cν.(τ, Sν_lin, an...)
+C2 = Cν.(τ, Sν_quad, an...)
