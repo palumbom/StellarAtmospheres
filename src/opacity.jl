eb6093a558ca62f1e18f423358980c599f42d150
Michael Palumbo <mlp95@psu.edu>
2020-02-18 12:02:01 -0600

    replace nothing with nan

--- src/opacity.jl
+++ src/opacity.jl
@@ -10,18 +10,31 @@ function tryparse(Float64, x::Float64)
     return x
 end
 
+function col_to_float!(df::DataFrame, colnames::T...) where T<:Symbol
+    for col in colnames
+        df[!, col] = tryparse.(Float64, df[!, col])
+        df[isnothing.(df[!, col]), col] .= NaN
+    end
+    return df
+end
+
 """
 
 """
 function tabulate_partition(dir::String=datdir)
     @assert isdir(dir)
 
-    # read in the files
+    # read in the files & # make sure numbers are floats
     df = CSV.read(dir * "partit.txt", header=0)
+    col_to_float!(df, names(df)[2:end]...)
+    return df
+end
 
-    # loop over rows
-    for i in 2:length(names(df))
-        df[!, names(df)[i]] = tryparse.(Float64, df[!, names(df)[i]])
-    end
+function tabulate_ionization(dir::String=datdir)
+    @assert isdir(dir)
+
+    # read in the files & # make sure numbers are floats
+    df = CSV.read(dir * "ioniz.txt", header=0)
+    col_to_float!(df, names(df)[2:end]...)
     return df
 end
