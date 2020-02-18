d780d4a1ae08d5f3e23ced317eb44af617bd52bc
Michael Palumbo <mlp95@psu.edu>
2020-02-18 11:43:09 -0600

    partition tabulation

--- src/opacity.jl
+++ src/opacity.jl
@@ -2,11 +2,26 @@
 using CSV
 using DataFrames
 
-datdir = "../data/"
+datdir = (@__DIR__) * "/../data/"
 
-function tabulate_partition(dir::String)
+# extend tryparse function
+import Base.tryparse
+function tryparse(Float64, x::Float64)
+    return x
+end
+
+"""
+
+"""
+function tabulate_partition(dir::String=datdir)
     @assert isdir(dir)
 
+    # read in the files
+    df = CSV.read(dir * "partit.txt", header=0)
 
-    return
+    # loop over rows
+    for i in 2:length(names(df))
+        df[!, names(df)[i]] = tryparse.(Float64, df[!, names(df)[i]])
+    end
+    return df
 end
