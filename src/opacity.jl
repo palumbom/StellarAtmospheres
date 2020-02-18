2d6d379152913937754595160a6292c5f18ab5ed
Michael Palumbo <mlp95@psu.edu>
2020-02-18 11:00:07 -0600

    ionization etc. data

---
+++ src/opacity.jl
@@ -0,0 +1,12 @@
+# modules for reading/writing table data
+using CSV
+using DataFrames
+
+datdir = "../data/"
+
+function tabulate_partition(dir::String)
+    @assert isdir(dir)
+
+
+    return
+end
