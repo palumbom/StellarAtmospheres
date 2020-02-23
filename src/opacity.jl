3939a4041b7416921887649786adf5b663f97028
Michael Palumbo <mlp95@psu.edu>
2020-02-23 13:16:17 -0500

    tweak

--- src/opacity.jl
+++ src/opacity.jl
@@ -38,9 +38,8 @@ function tabulate_partition(dir::String=datdir)
     @assert isdir(dir)
 
     # read in the files & # make sure numbers are floats
-    num1 = "θ_0" .* string.(range(2, 8, step=2))
-    num2 = "θ_" .* string.(range(10, 20, step=2))
-    header = ["Species", num1..., num2..., "logg0"]
+    num = string.(range(0.2, 2.0, step=0.2))
+    header = ["Species", num..., "logg0"]
     df = CSV.read(dir * "partit.txt", header=header, silencewarnings=true)
     col_to_float!(df, names(df)[2:end]...)
     return df
